461bf9bedb1f9f119953f57795751b941bda9420
aornota <djnarration@gmail>
2019-11-15 14:12:49 +0000

    Work-in-progress.

--- src/common/project-dependency-data.fs
+++ src/common/project-dependency-data.fs
@@ -2,7 +2,7 @@ module Aornota.Duh.Common.ProjectDependencyData
 
 open Aornota.Duh.Common.Domain
 
-// #region Solutions
+// #region Solutions:
 
 let domainSln = { Name = "Domain" ; Repo = AzureDevOps ; RootPath = "source" ; Colour = Grey }
 let infrastructureSln = { Name = "Infrastructure" ; Repo = AzureDevOps ; RootPath = "source" ; Colour = SlateGrey }
@@ -31,19 +31,66 @@ let toolsProj = { Name = "Tools" ; Solution = sharedSln ; ExtraPath = Some "Non 
 
 // #region ProjectDependencies:
 
-let commonInterfacesDeps = { Project = commonInterfacesProj ; Dependencies = [] |> Set.ofList }
-let commonModelsDeps = { Project = commonModelsProj ; Dependencies = [ PackageReference commonInterfacesProj ] |> Set.ofList }
-let commonExtensionsDeps = { Project = commonExtensionsProj ; Dependencies = [ PackageReference commonModelsProj ] |> Set.ofList }
-let productInterfacesDeps = { Project = productInterfacesProj ; Dependencies = [ PackageReference commonInterfacesProj ] |> Set.ofList }
-let productModelsDeps = { Project = productModelsProj ; Dependencies = [ PackageReference productInterfacesProj ; PackageReference commonModelsProj ] |> Set.ofList }
-let orderInterfacesDeps = { Project = orderInterfacesProj ; Dependencies = [ PackageReference productInterfacesProj ] |> Set.ofList }
-let orderModelsDeps = { Project = orderModelsProj ; Dependencies = [ PackageReference orderInterfacesProj ; PackageReference productModelsProj ] |> Set.ofList }
-let infrastructureInterfacesDeps = { Project = infrastructureInterfacesProj ; Dependencies = [ PackageReference orderInterfacesProj ] |> Set.ofList }
+let commonInterfacesDeps = {
+    Project = commonInterfacesProj
+    Dependencies = [] |> Set.ofList }
+let commonModelsDeps = {
+    Project = commonModelsProj
+    Dependencies = [
+        PackageReference commonInterfacesProj
+    ] |> Set.ofList }
+let commonExtensionsDeps = {
+    Project = commonExtensionsProj
+    Dependencies = [
+        PackageReference commonModelsProj
+    ] |> Set.ofList }
+let productInterfacesDeps = {
+    Project = productInterfacesProj
+    Dependencies = [
+        PackageReference commonInterfacesProj
+    ] |> Set.ofList }
+let productModelsDeps = {
+    Project = productModelsProj
+    Dependencies = [
+        PackageReference productInterfacesProj
+        PackageReference commonModelsProj
+    ] |> Set.ofList }
+let orderInterfacesDeps = {
+    Project = orderInterfacesProj
+    Dependencies = [
+        PackageReference productInterfacesProj
+    ] |> Set.ofList }
+let orderModelsDeps = {
+    Project = orderModelsProj
+    Dependencies = [
+        PackageReference orderInterfacesProj
+        PackageReference productModelsProj
+    ] |> Set.ofList }
+let infrastructureInterfacesDeps = {
+    Project = infrastructureInterfacesProj
+    Dependencies = [
+        PackageReference orderInterfacesProj
+    ] |> Set.ofList }
 
-let repositoriesDeps = { Project = repositoriesProj ; Dependencies = [ PackageReference infrastructureInterfacesProj ; PackageReference orderModelsProj ] |> Set.ofList }
-let repositoriesTestsDeps = { Project = repositoriesTestsProj ; Dependencies = [ PackageReference commonExtensionsProj ; ProjectReference repositoriesProj ] |> Set.ofList }
+let repositoriesDeps = {
+    Project = repositoriesProj
+    Dependencies = [
+        PackageReference infrastructureInterfacesProj
+        PackageReference orderModelsProj
+    ] |> Set.ofList }
+let repositoriesTestsDeps = {
+    Project = repositoriesTestsProj
+    Dependencies = [
+        PackageReference commonExtensionsProj
+        ProjectReference repositoriesProj
+    ] |> Set.ofList }
 
-let toolsDeps = { Project = toolsProj ; Dependencies = [ PackageReference orderModelsProj ; PackageReference commonExtensionsProj ] |> Set.ofList }
+let toolsDeps = {
+    Project = toolsProj
+    Dependencies = [
+        PackageReference orderModelsProj
+        PackageReference commonExtensionsProj
+    ] |> Set.ofList }
 
 let projectsDependencies= [
     commonInterfacesDeps ; commonModelsDeps ; commonExtensionsDeps
